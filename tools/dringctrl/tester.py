8f1cf31656bfd7bf2f1c0b8db8ef4e9a35f7ebc9
Reese Wilson <3253971+figadore@users.noreply.github.com>
2020-06-22 15:41:44 -0700

    fix config test

--- tools/dringctrl/tester.py
+++ tools/dringctrl/tester.py
@@ -55,7 +55,7 @@ class DRingTester():
     codecAudio = ''
     codecVideo = ''
 
-    def testConfig(self, ctrl):
+    def testConfig(self, ctrl, nbIteration, delay):
         print("**[BEGIN] test config")
         allCodecs = ctrl.getAllCodecs()
         if len(allCodecs) == 0:
